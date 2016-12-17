fb9fff0f93b45ce30006fb6216789fc29f7aa378
haruda <harudagondi@hotmail.com>
2016-12-17 18:13:26 +0800

    added app.py

---
+++ app.py
@@ -0,0 +1,32 @@
+import yaml, tweepy, markovify
+
+config_file = "config.yaml"
+COUNT = 1000
+USERS = ["haruda02"]
+
+with open(config_file, "r") as f:
+    config = yaml.load(f)
+
+auth = tweepy.OAuthHandler(
+    config["CONSUMER_KEY"],
+    config["CONSUMER_SECRET"]
+)
+auth.set_access_token(
+    config["ACCESS_TOKEN"],
+    config["ACCESS_SECRET"]
+)
+api = tweepy.API(auth)
+
+def make_a_tweet():
+    tweets = []
+
+    for user in USERS:
+        timeline = api.user_timeline(id=user, count=COUNT)
+        tweets.extend([tweet.text.encode("utf-8") for tweet in timeline])
+
+    tweets = b"\n".join(tweets)
+
+    return markovify.NewlineText(tweets.decode("utf-8")).make_short_sentence(140)
+
+if __name__ == "__main__":
+    api.update_status(make_a_tweet())
